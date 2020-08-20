19a56084a752d27cebda33aec6e1d18778628d3b
gloria <cmxgloria@gmail.com>
2020-08-20 16:39:10 +1000

    created homepage special contact header components and updated app.jsx route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/header/Header.jsx";
+import Special from "./components/special/Special.jsx";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import Contact from "./components/contact/Contact.jsx";
+import HomePage from "./components/homePage/HomePage.jsx";
+// import ShopPage from "./pages/shop/ShopPage.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Header />
+        </header>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route path="/shop" component={ShopPage} /> */}
+          <Route path="/special" component={Special} />
+          <Route path="/contact" component={Contact} />
+          {/* <Route path="/handmade-art" component={HandmadeArt} /> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/special/Special.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Special() {
+  return (
+    <div>
+      <h1>boomeranges on sale January and February</h1>
+      <h3>Get a Free Gift with Code 888</h3>
+    </div>
+  );
+}
