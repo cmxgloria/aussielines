da602c95eeb3bdb81761aab0bdcd81cffa444812
gloria <cmxgloria@gmail.com>
2020-08-21 21:31:41 +1000

    updated shopphotos css

--- src/components/shopPhotos/ShopPhotos.jsx
+++ src/components/shopPhotos/ShopPhotos.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import stall2 from "../photos/stall2.jpg";
+import stall1 from "../photos/stall1.jpg";
+import PLATE4 from "../photos/PLATE4.jpg";
+
 import "./shop-photos.styles.scss";
 
 function ShopPhotos() {
   return (
     <div className="shop-header">
       <div className="shop-photos">
-        <img src="https://imgur.com/pUoFaEi" alt="" />
+        <img src={stall2} alt="stall2" />
+        <img src={stall1} alt="stall1" />
+        <img src={PLATE4} alt="plate" />
       </div>
       <p>
         Aussielines -- Australian Souvenirs in Queen Victoria Market Melbourne
--- src/components/special/Special.jsx
+++ src/components/special/Special.jsx
@@ -5,7 +5,6 @@ export default function Special() {
   return (
     <div className="special">
       <h2>SALE</h2>
-
       <h1>Boomeranges on Sale January and February</h1>
       <h3>Get a Free Gift with Code888</h3>
     </div>
