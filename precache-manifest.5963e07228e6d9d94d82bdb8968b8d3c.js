af8097a54eaa15ba5d1d24a899334492f73ba94d
gloria <cmxgloria@gmail.com>
2020-08-24 12:18:16 +1000

    Updates

---
+++ precache-manifest.5963e07228e6d9d94d82bdb8968b8d3c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "556510bdf5abf7f13fe2548bd19f0c97",
+    "url": "/aussielines/index.html"
+  },
+  {
+    "revision": "ab3fc439083f35742f43",
+    "url": "/aussielines/static/css/main.ea7bfd2a.chunk.css"
+  },
+  {
+    "revision": "6bf73b097210c6f823fb",
+    "url": "/aussielines/static/js/2.1ce7ce6d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/aussielines/static/js/2.1ce7ce6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab3fc439083f35742f43",
+    "url": "/aussielines/static/js/main.e8b097ae.chunk.js"
+  },
+  {
+    "revision": "315a76e128f0b98c3747",
+    "url": "/aussielines/static/js/runtime-main.743e5180.js"
+  },
+  {
+    "revision": "da98986bc6a1b0f77df2d665421462f3",
+    "url": "/aussielines/static/media/PLATE4.da98986b.jpg"
+  },
+  {
+    "revision": "e37b33e603d9dc2170634eee0e8d7a39",
+    "url": "/aussielines/static/media/Screen Shot 2020-08-22 at 12.21.38 pm.e37b33e6.png"
+  },
+  {
+    "revision": "d161b374c2dbcc2634d86cda11e65801",
+    "url": "/aussielines/static/media/Screen Shot 2020-08-22 at 12.32.35 pm.d161b374.png"
+  },
+  {
+    "revision": "7a32607b73a2212e341c0c9db41a6901",
+    "url": "/aussielines/static/media/central-station-2661255__480.7a32607b.jpg"
+  },
+  {
+    "revision": "d0781dfad55d753b0cffc6b4357959bf",
+    "url": "/aussielines/static/media/sign blue.d0781dfa.jpg"
+  },
+  {
+    "revision": "b4bdb99e836dc20531cc6da7e157ae1d",
+    "url": "/aussielines/static/media/stall2.b4bdb99e.jpg"
+  }
+]);
