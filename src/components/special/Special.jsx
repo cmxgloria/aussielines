108f5bff8dd10cb0e58df038d87809a598892e53
gloria <cmxgloria@gmail.com>
2020-09-25 17:11:58 +1000

    fixed contact and special on mobile version

--- src/components/special/Special.jsx
+++ src/components/special/Special.jsx
@@ -6,7 +6,7 @@ export default function Special() {
     <div className="special">
       <h2>SALE</h2>
       <h1>Boomeranges on Sale January and February</h1>
-      <h3>Get a Free Gift with Code888</h3>
+      {/* <p>Get a Free Gift with Code888</p> */}
     </div>
   );
 }
